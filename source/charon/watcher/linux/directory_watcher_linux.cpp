cce63756569d4ea840029ee1d6170d11ed6ad88c
MaciejDziuban <dziuban.maciej@gmail.com>
2021-11-21 15:00:52 +0100

    Linux 3/n - implement DirectoryWatcherLinux

--- source/charon/watcher/linux/directory_watcher_linux.cpp
+++ source/charon/watcher/linux/directory_watcher_linux.cpp
@@ -1,5 +1,11 @@
+#include "charon/util/linux/error.h"
 #include "charon/watcher/directory_watcher_factory.h"
 #include "charon/watcher/linux/directory_watcher_linux.h"
+
+#include <pthread.h>
+#include <signal.h>
+#include <sys/inotify.h>
+#include <unistd.h>
 
 std::unique_ptr<DirectoryWatcher> DirectoryWatcherFactoryImpl::create(const std::filesystem::path &directoryPath,
                                                                       FileEventQueue &outputQueue,
@@ -17,13 +23,111 @@ DirectoryWatcherLinux::~DirectoryWatcherLinux() {
 }
 
 bool DirectoryWatcherLinux::isWorking() const {
-    return false;
+    return watcherThreadWorking.load();
 }
 
 bool DirectoryWatcherLinux::start() {
+    if (isWorking()) {
+        return false;
+    }
+
+    inotifyEventQueue = inotify_init();
+    FATAL_ERROR_IF_SYSCALL_FAILED(inotifyEventQueue, "Failed inotify_init");
+    inotifyWatchDescriptor = inotify_add_watch(inotifyEventQueue, directoryPath.c_str(), IN_CREATE | IN_DELETE | IN_MOVE | IN_MODIFY);
+    FATAL_ERROR_IF_SYSCALL_FAILED(inotifyWatchDescriptor, "Failed inotify_add_watch");
+
+    // Start background thread
+    this->watcherThread = std::make_unique<std::thread>(watcherThreadProcedure, std::reference_wrapper{*this});
+    while (!isWorking())
+        ;
+
     return true;
 }
 
 bool DirectoryWatcherLinux::stop() {
+    if (!isWorking()) {
+        return false;
+    }
+
+    // Interrupt background thread and wait for completion
+    FATAL_ERROR_IF_SYSCALL_FAILED(pthread_kill(watcherThread->native_handle(), SIGUSR1), "Failed pthread_kill");
+    watcherThread->join();
+
+    // Verify the thread marked its end
+    FATAL_ERROR_IF(isWorking(), "Watcher thread is still working after kill");
+
+    // Cleanup inotify queue
+    FATAL_ERROR_IF_SYSCALL_FAILED(inotify_rm_watch(inotifyEventQueue, inotifyWatchDescriptor), "Failed inotify_rm_watch");
+    FATAL_ERROR_IF_SYSCALL_FAILED(close(inotifyEventQueue), "Failed closing inotify queue");
+
+    // Cleanup thread reference
+    watcherThread = nullptr;
+
+    return true;
+}
+
+void DirectoryWatcherLinux::watcherThreadProcedure(DirectoryWatcherLinux &watcher) {
+    constexpr size_t bufferSize = sizeof(inotify_event) * 256;
+    auto buffer = std::make_unique<std::byte[]>(bufferSize);
+
+    // We will use SIGUSR1 signal to interrupt read() operation in this thread.
+    // We have to still register a signal handler, so the thread is not killed.
+    struct sigaction h = {};
+    h.sa_handler = dummySignalHandler;
+    sigaction(SIGUSR1, &h, 0);
+
+    watcher.watcherThreadWorking.store(true);
+    while (true) {
+        const ssize_t readResult = read(watcher.inotifyEventQueue, buffer.get(), bufferSize);
+
+        // Handle thread interruption
+        if (readResult == -1 && errno == EINTR) {
+            watcher.watcherThreadWorking.store(false);
+            return;
+        }
+
+        // Handle other errors
+        FATAL_ERROR_IF_SYSCALL_FAILED(readResult, "Read from inotify queue failed");
+
+        // We received valid data from inotify and we have to process them
+        for (ssize_t positionInBuffer = 0; positionInBuffer < readResult;) {
+            const inotify_event &inotifyEvent = reinterpret_cast<inotify_event &>(buffer[positionInBuffer]);
+
+            FileEvent fileEvent{};
+            if (watcher.createFileEvent(inotifyEvent, fileEvent)) {
+                if (fileEvent.needsFileLocking()) {
+                    watcher.deferredOutputQueue.push(std::move(fileEvent));
+                } else {
+                    watcher.outputQueue.push(std::move(fileEvent));
+                }
+            }
+
+            positionInBuffer += sizeof(inotify_event) + inotifyEvent.len;
+        }
+    }
+}
+
+bool DirectoryWatcherLinux::createFileEvent(const inotify_event &inotifyEvent, FileEvent &outEvent) const {
+    if (inotifyEvent.mask & IN_ISDIR) {
+        return false;
+    }
+
+    FileEvent::Type type{};
+    if (inotifyEvent.mask & IN_CREATE) {
+        type = FileEvent::Type::Add;
+    } else if (inotifyEvent.mask & IN_DELETE) {
+        type = FileEvent::Type::Remove;
+    } else if (inotifyEvent.mask & IN_MOVED_FROM) {
+        type = FileEvent::Type::RenameOld;
+    } else if (inotifyEvent.mask & IN_MOVED_TO) {
+        type = FileEvent::Type::RenameNew;
+    } else if (inotifyEvent.mask & IN_MODIFY) {
+        type = FileEvent::Type::Modify;
+    } else {
+        return false;
+    }
+
+    const std::string path{inotifyEvent.name};
+    outEvent = FileEvent{this->directoryPath, type, this->directoryPath / path};
     return true;
 }
--- source/charon/watcher/linux/directory_watcher_linux.h
+++ source/charon/watcher/linux/directory_watcher_linux.h
@@ -2,6 +2,10 @@
 
 #include "charon/watcher/directory_watcher.h"
 
+#include <atomic>
+
+struct inotify_event;
+
 class DirectoryWatcherLinux : public DirectoryWatcher {
 public:
     DirectoryWatcherLinux(const std::filesystem::path &directoryPath, FileEventQueue &outputQueue, FileEventQueue &deferredOutputQueue);
@@ -10,4 +14,14 @@ public:
     bool start() override;
     bool stop() override;
     bool isWorking() const override;
+
+private:
+    static void watcherThreadProcedure(DirectoryWatcherLinux &watcher);
+    static void dummySignalHandler(int) {}
+    bool createFileEvent(const inotify_event &inotifyEvent, FileEvent &outEvent) const;
+
+    OsHandle inotifyEventQueue = defaultOsHandle;
+    OsHandle inotifyWatchDescriptor = defaultOsHandle;
+    std::unique_ptr<std::thread> watcherThread = nullptr;
+    std::atomic_bool watcherThreadWorking = false;
 };
